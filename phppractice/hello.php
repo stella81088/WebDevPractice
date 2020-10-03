d0ae70e056389593310be9f70c6277a94dd55a70
Stella81088 <sli@jlisbz.com>
2020-10-02 21:08:31 -0400

    playing around with basic css style sheet

--- phppractice/hello.php
+++ phppractice/hello.php
@@ -1,3 +1,16 @@
-<?php
- echo "HELLO WORLD";
-?>
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title> CSS playground </title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+
+    <body>
+        <h1 class="blue">Introduction to CSS</h1>
+        <p> This is just a quick intro to CSS </p>
+
+        <div class="box"></div>
+    </body>
+</html>
